dbe1a7436066085e500fba177e8d169e244b119a
Sanket Kolte <121600277+Sanketk55@users.noreply.github.com>
2024-01-31 20:25:39 +0530

    Add files via upload

---
+++ Insert and Search in a Trie/insert.cpp
@@ -0,0 +1,30 @@
+// User function template for C++
+
+// trie node
+/*
+struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isLeaf is true if the node represents
+    // end of a word
+    bool isLeaf;
+};
+*/
+class Solution
+{
+    public:
+        //Function to insert string into TRIE.
+        unordered_map<string , bool>mp;
+        void insert(struct TrieNode *root, string key)
+        {
+            // code here
+            mp[key]=true;
+        }
+        
+        //Function to use TRIE data structure and search the given string.
+        bool search(struct TrieNode *root, string key) 
+        {
+            // code here
+            return mp[key];
+        }
+};
