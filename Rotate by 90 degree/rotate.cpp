a35ddf09a6405d4e71717a9b97aeea090bb72ce5
Sanket Kolte <121600277+Sanketk55@users.noreply.github.com>
2024-02-23 13:19:45 +0530

    Add files via upload

---
+++ Rotate by 90 degree/rotate.cpp
@@ -0,0 +1,15 @@
+//User function template for C++
+
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    // Your code goes here
+    int n = matrix.size();
+    for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    reverse(matrix.begin(), matrix.end());
+}
