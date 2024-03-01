fe8923d6d04f2baf93db2268e6f7750a30b9aff3
Sanket Kolte <121600277+Sanketk55@users.noreply.github.com>
2024-03-01 18:18:59 +0530

    Add files via upload

---
+++ Peak element/peakElement.cpp
@@ -0,0 +1,36 @@
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+       if(n==1){
+           return 0;
+       }
+       if(arr[0] >= arr[1]) return 0;
+       if(arr[n-1] >= arr[n-2]) return n-1;
+       
+       int low = 1;
+       int high = n-2;
+       
+       while(low <= high){
+           int mid = low + (high - low)/2;
+           if(arr[mid] >= arr[mid+1] && arr[mid] >= arr[mid-1]){
+               return mid;
+           }
+           else if (arr[mid] >= arr[mid-1]){
+               low = mid + 1;
+           }
+           else{
+               high = mid - 1;
+           }
+       }
+       return -1;
+       
+    }
+};
